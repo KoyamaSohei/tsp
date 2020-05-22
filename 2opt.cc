fac7f217993e24edd4e6f28e8827380aab3fb8af
KoyamaSohei <koyamaso0309@gmail.com>
2020-05-22 09:20:00 +0900

    update 2-opt

--- 2opt.cc
+++ 2opt.cc
@@ -1,4 +1,8 @@
 #include <bits/stdc++.h>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/index/rtree.hpp>
 #define MAX 20000
 #define rep(i,n) for(int i = 0; i < (n); ++i)
 #define drep(i,n) for(int i = (n)-1; i >= 0; --i)
@@ -13,12 +17,43 @@ typedef vector<int> vi;
 typedef vector<vi> vvi;
 const int INF = 1001001001;
 
+namespace bg = boost::geometry;
+namespace bgi = boost::geometry::index;
+typedef bg::model::point<float, 2, bg::cs::cartesian> point;
+
 extern int n, length, tour[MAX];
 extern float city[MAX][2];
 extern void showTour(int *tou, int wai, int color);
 extern void showCTour(int *tou, int wai, int *color);
 extern void showString(char *str);
 extern void showLength(int leng);
+
+class UnionFind {
+  public:
+    vi Parent;
+    UnionFind(int N) {
+      Parent = vi(N,-1);
+    }
+  
+    int root(int A) {
+      if(Parent[A] < 0) return A;
+      return Parent[A] = root(Parent[A]);
+    }
+  
+    int size(int A) {
+      return -Parent[root(A)];
+    }
+  
+    bool connect(int A,int B) {
+      A = root(A);
+      B = root(B);
+      if(A == B) return false;
+      if(size(A) < size(B)) swap(A,B);
+      Parent[A] += Parent[B];
+      Parent[B] = A;
+      return true;
+    }
+};
 
 // http://xoshiro.di.unimi.it/xoshiro256starstar.c
 
@@ -51,15 +86,71 @@ int dist(int i, int j) {
   return (int)(sqrt(xd * xd + yd * yd) + .5);
 }
 
+UnionFind uf(MAX);
+
+bgi::rtree<pair<point,unsigned>,bgi::quadratic<MAX>> rtree;
+typedef vector<pair<point,unsigned>> vp;
+
+int getNextPoint(int k) {
+  point p(city[k][0],city[k][1]);
+  int num=2;
+  while(1) {
+    vp dst;
+    rtree.query(bgi::nearest(p,num),back_inserter(dst));
+    for(auto nea:dst) {
+      int id = nea.second;
+      if(id==k) continue;
+      if(uf.root(id)==uf.root(k)) continue;
+      uf.connect(id,k);
+      return id;
+    }
+    num <<= 1;
+  }
+  return -1;
+}
+
+vi neighbor10[MAX];
 
 // TODO: そこそこ良い構築をする
 void build() {
-  length=0;
-  rep(i,n) {
-    tour[i]=i;
+  {
+    // build rtree
+    rep(i,n) {
+      point p(city[i][0],city[i][1]);
+      rtree.insert(make_pair(p,i));
+    }
   }
-  rep(i,n) {
-    length+=dist(i,(i+1)%n);
+  {
+    // build initial path
+    vi next(n,-1);
+    int k=n-1;
+    rep(i,n-1) {
+      int nex = getNextPoint(k);
+      next[k]=nex;
+      k=nex;
+    }
+    next[k]=n-1;
+    k=n-1;
+    rep(i,n) {
+      tour[i]=k;
+      k=next[k];
+    }
+    rep(i,n) {
+      length += dist(tour[i],tour[(i+1)%n]);
+    }
+  }
+  {
+    // build neighbor10
+    rep(i,n) {
+      point p(city[i][0],city[i][1]);
+      vp dst;
+      rtree.query(bgi::nearest(p,max(n,11)),back_inserter(dst));
+      for(auto nea:dst) {
+        int id = nea.second;
+        if(id==i) continue;
+        neighbor10[i].push_back(id);
+      }
+    }
   }
 }
 
@@ -82,13 +173,15 @@ void twoopt() {
   rep(i,n) {
     int a = tour[i];
     int b = tour[(i+1)%n];
-    int k = (next())%n;// TODO: 近くを選びやすくする
+    int k = neighbor10[a][(next())%sz(neighbor10[a])];
     int c = tour[k];
     if(b==c) continue;
     int d = tour[(k+1)%n];
     if(b==d||a==d) continue;
     int tmp = dist(a,b)+dist(c,d)-dist(a,c)-dist(b,d);
     if(tmp>0) {
+      show(i);
+      show(k);
       flip(i,(i+1),k,(k+1)%n);
       length-=tmp;
       return;
