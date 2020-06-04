d8be5dbff152c84f34448d371e6161d711ba4dfe
KoyamaSohei <koyamaso0309@gmail.com>
2020-06-05 03:14:55 +0900

    refactor

--- genans.cc
+++ genans.cc
@@ -19,59 +19,68 @@ int main(int argc,char *argv[]) {
     exit(1);
   }
   FILE *fp;
-  // open .tsp 
-  
-  char tsp[100];
-  sprintf(tsp,"./lib/%s.tsp",argv[1]);
-  if((fp = fopen(tsp,"r")) == NULL) {
-    fprintf(stderr, "cannot open file!\n");
-    exit(1);
-  }
   char buf[1024];
   int dim;
-  while(fgets(buf,1024,fp) != NULL) {
-    if(strncmp("DIMENSION", buf, 9)==0) {
-      char *c1;
-      for(c1 = buf + 9; *c1 != ':'; c1++); 
-      dim = atoi(c1+1);
-      continue;
+  vi xs,ys;
+  {
+    // read data from %s.tsp
+    char tsp[100];
+    sprintf(tsp,"./lib/%s.tsp",argv[1]);
+    if((fp = fopen(tsp,"r")) == NULL) {
+      fprintf(stderr, "cannot open file!\n");
+      exit(1);
     }
-    if(strncmp("NODE_COORD_SECTION", buf, 18)==0) break;
+    while(fgets(buf,1024,fp) != NULL) {
+      if(strncmp("DIMENSION", buf, 9)==0) {
+        char *c1;
+        for(c1 = buf + 9; *c1 != ':'; c1++); 
+        dim = atoi(c1+1);
+        continue;
+      }
+      if(strncmp("NODE_COORD_SECTION", buf, 18)==0) break;
+    }
+    xs.resize(dim);
+    ys.resize(dim);
+    while(fgets(buf,1024,fp) != NULL) {
+      if(strncmp("EOF", buf, 3)==0) break;
+      char *p = buf;
+      int k = strtol(p,&p,10);
+      int x = strtol(p,&p,10);
+      int y = strtol(p,&p,10);
+      xs[k-1]=x;
+      ys[k-1]=y;
+    }
+    fclose(fp);
   }
+  {
+    // read data from %s.opt.tour
+    
+    char tour[100];
+    sprintf(tour,"./lib/%s.opt.tour",argv[1]);
+    if((fp = fopen(tour,"r")) == NULL) {
+      fprintf(stderr, "cannot open file!\n");
+      exit(1);
+    }
+    int dim2;
+    while(fgets(buf,1024,fp) != NULL) {
+      if(strncmp("DIMENSION", buf, 9)==0) {
+        char *c1;
+        for(c1 = buf + 9; *c1 != ':'; c1++); 
+        dim2 = atoi(c1+1);
+        continue;
+      }
+      if(strncmp("TOUR_SECTION", buf, 12)==0) break;
+    }
 
-  vi xs(dim),ys(dim);
-  while(fgets(buf,1024,fp) != NULL) {
-    if(strncmp("EOF", buf, 3)==0) break;
-    char *p = buf;
-    int k = strtol(p,&p,10);
-    int x = strtol(p,&p,10);
-    int y = strtol(p,&p,10);
-    xs[k-1]=x;
-    ys[k-1]=y;
-  }
-  fclose(fp);
-  
-  
-  char tour[100];
-  sprintf(tour,"./lib/%s.opt.tour",argv[1]);
-  if((fp = fopen(tour,"r")) == NULL) {
-    fprintf(stderr, "cannot open file!\n");
-    exit(1);
-  }
-  int dim2;
-  while(fgets(buf,1024,fp) != NULL) {
-    if(strncmp("DIMENSION", buf, 9)==0) {
-      char *c1;
-      for(c1 = buf + 9; *c1 != ':'; c1++); 
-      dim2 = atoi(c1+1);
-      continue;
+    // verify if dim = dim2
+
+    if(dim!=dim2) {
+      printf("dimension(tsp: %d, tour: %d) is invalid!\n",dim,dim2);
+      fclose(fp);
+      exit(1);
     }
-    if(strncmp("TOUR_SECTION", buf, 12)==0) break;
   }
-  if(dim!=dim2) {
-    printf("dimension(tsp: %d, tour: %d) is invalid!\n",dim,dim2);
-    exit(1);
-  }
+  // print data
   cout << dim << endl;
   while(fgets(buf,1024,fp) != NULL) {
     if(strncmp("-1", buf, 2)==0) break;
