430d3012c5cee1aef9a2fc8d6cf106c0788558de
KoyamaSohei <koyamaso0309@gmail.com>
2020-07-14 20:50:22 +0900

    remove test for check config worked

--- util/evaluate.sh
+++ util/evaluate.sh
@@ -2,7 +2,7 @@
 
 a=("ni" "nn" "fi" "greedy")
 b=("2opt" "sa" "tabu" "combopt")
-c=("ulysses22" "pr107" "a280" "u574" "u1060" "u2319" "rl5915" "usa13509")
+c=("ulysses22")
 d=("2" "20")
 
 for k in "${c[@]}"
