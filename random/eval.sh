4b3b4ac47825c10f11b26095bca7bb2adc0377bf
KoyamaSohei <koyamaso0309@gmail.com>
2020-07-25 09:14:24 +0900

    random: add test

--- random/eval.sh
+++ random/eval.sh
@@ -4,7 +4,7 @@ for i in "${a[@]}"
 do
   echo "build ${i}.cc"
   eval "g++ -O2 -I/usr/include/X11/ -L/usr/lib/ -lX11 -lm util/tspShow.c ${i}.cc -o ${i}.out"
-  for j in {10..36}
+  for j in {10..45}
   do
     echo "target: ${j}"
     eval "time ./${i}.out -n -c random/v${j}.tsp | ./export.out > dist/v${j}.${i}.svg"
