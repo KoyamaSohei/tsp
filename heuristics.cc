809501b2f0ac3a8ce4c7f3431a70a7bee121976f
KoyamaSohei <koyamaso0309@gmail.com>
2020-07-26 14:26:57 +0900

    add: clock()

--- heuristics.cc
+++ heuristics.cc
@@ -1,10 +1,19 @@
+#include <time.h>
+#include <iostream>
+using namespace std;
+
 extern void bootstrap();
 extern void build();
 extern void run();
 
 int tspSolver() {
   bootstrap();
+  auto a = clock();
   build();
+  auto b = clock();
   run();
+  auto c = clock();
+  cerr << "build duration "   << (b-a)/CLOCKS_PER_SEC << endl;
+  cerr << "improve time " << (c-b)/CLOCKS_PER_SEC << endl;
   return 1;
 }
